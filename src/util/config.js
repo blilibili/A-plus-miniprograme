0c63958fe3b4ab24f9645e6769af8a74763b2ed6
unknown <daniel.li@7road.com>
2018-12-26 16:21:26 +0800

    20181226

---
+++ src/util/config.js
@@ -0,0 +1,25 @@
+module.exports = {
+    egsImg:'../img/egsIcon.jpg',
+    cieImg:'../img/cieIcon.jpg',
+    cimImg:'../img/cimIcon.jpg',
+    cirImg:'../img/cirIcon.jpg',
+    indexBanner:['../img/banner-1.jpg' , '../img/banner-one.jpg'],
+    formBg:'../../img/formBg.jpg',
+
+    egsBg:'../../img/egsProBg.jpg',
+    cieBg:'../../img/cieProBg.jpg',
+    cimBg:'../../img/cimProBg.jpg',
+    cirBg:'../../img/cirProBg.jpg',
+
+    feedBg:'../../img/cirProBg.jpg',
+
+    proTitle:{
+        EGS:'[富足三代]保障计划',
+        CIE:'危疾终身加护保',
+        CIM:'危疾终身加倍保',
+        CIR:'危疾终身全护保'
+    },
+
+    //是否显示重做计划书按钮
+    isShowRebuildPlain:true
+}
